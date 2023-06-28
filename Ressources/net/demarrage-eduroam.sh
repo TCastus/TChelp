b182089d8f6031216e9baa907b957a0a3e0b993f
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2023-06-28 18:09:24 +0200

    Mise à jour installations vmware, chrome, openconnect

--- Ressources/net/demarrage-eduroam.sh
+++ Ressources/net/demarrage-eduroam.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-echo "Editez ce script pour indiquer le chemin d'accès et le nom d'utilisateur"
+echo "Editez ce script pour préciser le nom d'utilisateur 'xxx' et le nom de l'interface réseau 'xxx'"
+echo "Le nom de l'interface s'obtient avec la commande ip"
 echo "...et décommentez les lignes"
 
-#nmcli con add type wifi con-name eduroam ssid eduroam ifname wlo1 -- 802-1x.eap peap 802-1x.identity xxx@insa-lyon.fr 802-1x.ca-cert file:////home/user/ca.pem 802-1x.phase2-auth mschapv2 802-11-wireless-security.key-mgmt wpa-eap 802-1x.password-flags 0x2
+#nmcli con add type wifi con-name eduroam ssid eduroam ifname xxx -- 802-1x.eap peap 802-1x.identity xxx@insa-lyon.fr 802-1x.ca-cert file:////home/user/ca.pem 802-1x.phase2-auth mschapv2 802-11-wireless-security.key-mgmt wpa-eap 802-1x.password-flags 0x2
 #nmcli co up eduroam --ask
