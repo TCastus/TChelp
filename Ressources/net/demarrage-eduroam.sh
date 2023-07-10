b80317bf5fd92724f2ec69a487ae12ff3f6cb260
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2023-07-10 12:35:32 +0200

    Mise à jour LiveCD

--- Ressources/net/demarrage-eduroam.sh
+++ Ressources/net/demarrage-eduroam.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-echo "Editez ce script pour préciser le nom d'utilisateur 'xxx' et le nom de l'interface réseau 'xxx'"
-echo "Le nom de l'interface s'obtient avec la commande ip : ex wlo1"
+echo "Editez ce script pour remplacer les xxx : nom utilisateur et interface"
+echo "Le nom de l'interface s'obtient avec la commande ip link : ex: wlo1"
 echo "...et décommentez les lignes"
+
+echo "En cas de bug, les commandes qui peuvent vous aider "
+echo "nmcli co show"
+echo "nmcli co delete eduroam"
 
 #nmcli con add type wifi con-name eduroam ssid eduroam ifname xxx -- 802-1x.eap peap 802-1x.identity xxx@insa-lyon.fr 802-1x.ca-cert file:////home/user/ca.pem 802-1x.phase2-auth mschapv2 802-11-wireless-security.key-mgmt wpa-eap 802-1x.password-flags 0x2
 #nmcli co up eduroam --ask
